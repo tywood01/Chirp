fe598edd6b27daa7da73ce17f858b00aabb258b8
Harvey Walter <harvey.walter@student.cune.edu>
2025-03-07 23:56:22 -0600

    Initial commit with HTML, CSS, and image

--- conf/settings.py
+++ conf/settings.py
@@ -132,3 +132,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "chirper/static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
