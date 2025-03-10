b25c289fb061fac5d24b2082e6bdd09160e8d146
Tytus Woodburn <tytus.woodburn@student.cune.edu>
2025-03-09 21:57:08 -0500

    Updated html formatting & admin-registered models.

--- chirper/admin.py
+++ chirper/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Chirp
+from .models import Chirp, Profile, Follow, Likes
 
 admin.site.register(Chirp)
+admin.site.register(Profile)
+admin.site.register(Follow)
+admin.site.register(Likes)
