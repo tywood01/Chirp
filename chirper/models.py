dee84125dcd30ecdde8e209b26f74d2bb73ab05f
tywood01 <tytus.woodburn@student.cune.edu>
2025-03-20 20:49:37 -0500

    Updated documentation on models and CSS

--- chirper/models.py
+++ chirper/models.py
@@ -16,6 +16,8 @@ from django.db import models
 
 
 class Chirp(models.Model):
+    """Represents a short message (chirp) in the Chirper app."""
+
     body = models.CharField(max_length=255)
     date = models.DateTimeField("Created At", auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -25,24 +27,28 @@ class Chirp(models.Model):
 
 
 class Profile(models.Model):
+    """Represents a user's profile and content about them."""
+
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     bio = models.CharField(max_length=255)
     public = models.BooleanField(default=True)
 
 
 class Likes(models.Model):
+    """Represents a user liking a post."""
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     chirp = models.ForeignKey(Chirp, on_delete=models.CASCADE, related_name="likes")
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["user", "chirp"], name="unique user chirp like"
-            )
-        ]
+        constraints = models.UniqueConstraint(
+            fields=["user", "chirp"], name="unique user chirp like"
+        )
 
 
 class Follow(models.Model):
+    """Represents a user following another user."""
+
     follower = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="following"
     )
@@ -51,8 +57,8 @@ class Follow(models.Model):
     )
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["follower", "followed"], name="unique follower followed"
-            )
-        ]
+        """One can't follow oneself."""
+
+        constraints = models.UniqueConstraint(
+            fields=["follower", "followed"], name="unique follower followed"
+        )
